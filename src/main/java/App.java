45aab1b3acae48fee9dbf12c0044e982cda271d9
Podgorka <andr02tagan@gmail.com>
2022-09-26 22:57:34 +0300

    initial

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+        ApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean3 = (Cat) applicationContext1.getBean("cats");
+        System.out.println(bean3.getMessage());
+        Cat bean4 = (Cat) applicationContext1.getBean("cats");
+        System.out.println(bean4.getMessage());
+
+        System.out.println(bean == bean2);
+        System.out.println(bean3 == bean4);
     }
 }
